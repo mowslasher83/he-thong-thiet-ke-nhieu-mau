6049d5c09c1fe02a1f7d77d3ca788ae141132cf3
mowslasher83 <radeswaled@hotmail.com>
2025-09-02 11:27:04 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from 'he-thong-thiet-ke-nhieu-mau';
+
+function App() {
+  return <Button color="primary">Nhấn vào đây</Button>;
+}
+
+export default App;
